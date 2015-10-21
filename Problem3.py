147ecd108bbed86ca8fa32f05feb667c8553f145
zakattak <zachary.jay.crabtree@gmail.com>
2015-10-21 09:29:01 -0700

    finished problem 3 :)

--- Problem3.py
+++ Problem3.py
@@ -6,6 +6,7 @@ import math
 #function to actually return the largest prime factor
 def findFactors(targetNum):
     #don't need to iterate higher than this
+    foundMaxFactor = False
     maxDivisor = int(targetNum**.5)+1
     maxFactor = 0
     #iterating through all numbers between 0 and target/2
@@ -15,15 +16,19 @@ def findFactors(targetNum):
             #and if the target is divisible by that prime number,
             #it's a prime factor!
             if(targetNum % x == 0):
-                print("A prime factor is: " + str(x))
-                print("The target was: " + str(targetNum))
-                print("The new target is: " + str(targetNum/x))
-                #if the target/factor is a prime, that means it's the largest prime factor
+                #print("A prime factor is: " + str(x))
+                #print("The target was: " + str(targetNum))
+                #print("The new target is: " + str(targetNum/x))
+                #if the target div by factor is a prime, that means it's the largest prime factor
                 if(isPrime(targetNum/x)):
                     print("The highest prime factor is: " + str(targetNum/x))
+                    foundMaxFactor = True
                     break
                 else:
                     findFactors(targetNum/x)
+                return foundMaxFactor
+
+
 
 #checking if a number is prime
 def isPrime(checkNum):
@@ -42,4 +47,4 @@ def isPrime(checkNum):
         return True
 
 findFactors(13195)
-#findFactors(600851475143)
+findFactors(600851475143)
